d1007b56d2ad21b72aee1febae697e3ac848d8f4
LeeJinheon <gcc.stu81@gcc.ai.kr>
2026-06-01 12:48:35 +0900

    feat: 3인칭 카메라 구현을 위한 SpringArm 컴포넌트 설정

---
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+ATPSPlayer::ATPSPlayer()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// TPS 카메라를 SpringArm 컴포넌트에 부착
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	springArmComp->SetupAttachment(RootComponent); // 계층 구조상 캡슐 컴포넌트가 ROOT이며 스프링암을 자식으로 배치
+	springArmComp->SetRelativeLocation(FVector(0.0f, 70.0f, 90.0f));
+	springArmComp->TargetArmLength = 400.0f; // 가까운 3인칭을 원하면 이걸 조절(원하는대로)
+		
+}
+
+// Called when the game starts or when spawned
+void ATPSPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATPSPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
