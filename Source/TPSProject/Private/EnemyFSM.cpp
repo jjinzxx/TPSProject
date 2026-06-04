6e4dc02446d9be517bb949a36113edddead510bd
LeeJinheon <gcc.stu81@gcc.ai.kr>
2026-06-04 16:26:49 +0900

    feat: EnemyFSM 이동 상태 구현(타겟 추적 + 공격 범위 진입 시 Attack으로 전이)

--- Source/TPSProject/Private/EnemyFSM.cpp
+++ Source/TPSProject/Private/EnemyFSM.cpp
@@ -3,6 +3,10 @@
 
 #include "EnemyFSM.h"
 
+#include "Enemy.h"
+#include "TPSPlayer.h"
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values for this component's properties
 UEnemyFSM::UEnemyFSM()
@@ -20,7 +24,12 @@ void UEnemyFSM::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	// 월드에서 ATPSPlayer 타겟 찾기
+	auto actor = UGameplayStatics::GetActorOfClass(GetWorld(), ATPSPlayer::StaticClass());
+	// 찾은 actor를 ATPSPlayer 타입으로 캐스팅
+	target = Cast<ATPSPlayer>(actor);
+	// 소유 객체
+	me = Cast<AEnemy>(GetOwner());
 	
 }
 
@@ -64,7 +73,18 @@ void UEnemyFSM::IdleState()
 // 추적 상태 - 플레이어 방향으로 이동, 공격 범위 진입 시 공격 상태로 전환
 void UEnemyFSM::MoveState()
 {
+	// 타겟 목적지 설정
+	FVector destination = target->GetActorLocation();
+	// 타겟 방향 설정
+	FVector direction = destination - me->GetActorLocation();
+	// 타겟 방향으로 이동
+	me->AddMovementInput(direction.GetSafeNormal());
 	
+	// Attack 상태로 전환 (공격범위 안으로 들어오면)
+	if (direction.Size() < attackRange)
+	{
+		mState = EEnemyState::Attack;
+	}
 }
 // 공격 상태 - 일정 주기로 공격, 플레이어가 범위 이탈 시 추적으로 전환
 void UEnemyFSM::AttackState()
--- Source/TPSProject/Public/EnemyFSM.h
+++ Source/TPSProject/Public/EnemyFSM.h
@@ -54,4 +54,16 @@ public:
 
 	// 경과 시간(누적)
 	float currentTime = 0.f;
+
+	// 목표 타겟
+	UPROPERTY(VisibleAnywhere, Category=FSM)
+	class ATPSPlayer* target;
+	
+	// 소유 벡터
+	UPROPERTY()
+	class AEnemy* me;
+	
+	// 공격 범위
+	UPROPERTY(EditDefaultsOnly, Category=FSM)
+	float attackRange = 150.f;
 };
