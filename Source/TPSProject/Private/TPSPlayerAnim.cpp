f6a6769274e8c792333c70399ddfe9b2ed6058c7
LeeJinheon <gcc.stu81@gcc.ai.kr>
2026-06-05 14:07:44 +0900

    feat: 애니메이션 연결

---
+++ Source/TPSProject/Private/TPSPlayerAnim.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayerAnim.h"
+
+#include "TPSPlayer.h"
+
+// 매 프레임 자동 호출 - Tick 직접 구성안해도 UE 자체가 호출
+void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	// Animation Instance를 소유한 폰(TPSPlayer) 가져오기
+	APawn* pawn = TryGetPawnOwner();
+	if (pawn==nullptr) return;
+	
+	// ATPSPlayer 폰의 속도 측정
+	ATPSPlayer* player = Cast<ATPSPlayer>(pawn);
+	if (player)
+	{
+		speed = player->GetVelocity().Size();
+	}
+
+	// [DEBUG] 프레임마다 변하는 speed 값 출력용
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 0.f, FColor::Green, 
+			FString::Printf(TEXT("Anim Speed: %f"), speed));
+	}
+}
---
+++ Source/TPSProject/Public/TPSPlayerAnim.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSPlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSPROJECT_API UTPSPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 매 프레임 자동 호출 - 소유한 폰의 상태를 읽어서 변수 갱신(tick 처럼 동작)
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	// 캐릭터의 현재 이동속도(ABP State Machine의 전이 조건에서 사용)
+	UPROPERTY(BlueprintReadOnly, Category=PlayerAnim)
+	float speed = 0.f;
+	
+	
+};
